bd6754170f23ed9c16b826ddbe5fd7bdb6dbb020
aminifard <87714732+aminifard@users.noreply.github.com>
2021-07-22 11:38:30 +0430

    Update optimizer.m

--- optimizer.m
+++ optimizer.m
@@ -2,12 +2,15 @@ clc
 clear 
 close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+m=input('Please enter the number of rows of matrix S:\n');
+n=input('Please enter the number of columns of matrix S:\n');
+d=input('Please enter the number of columns of matrix U:\n');
 filename = 'Book1.xlsx';
 sheet = 1;
 xlRange = 'A4:C27651';
 St = xlsread(filename,sheet,xlRange);
 [N,M]=size(St);
-s=zeros(4456,6663);
+s=zeros(m,n);
 for i=1:N
     s(St(i,1),St(i,2))=St(i,3);
 end
@@ -15,14 +18,14 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Lt=xlsread(filename,'A43736:C59813');
 [nl,ml]=size(Lt);
-l1=zeros(6663,30);
+l1=zeros(n,d);
 for i=1:nl
     l1(Lt(i,1),Lt(i,2))=Lt(i,3);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ut=xlsread(filename,'A27655:C43732');
 [nu,mu]=size(Ut);
-u1=zeros(6663,30);
+u1=zeros(n,d);
 for i=1:nu
     u1(Ut(i,1),Ut(i,2))=Ut(i,3);
 end
@@ -61,16 +64,8 @@ socConstraints(i+d)=secondordercone(a1,bl,D2,gamma);
 end
 [x,fval]=coneprog(f,socConstraints,A,b,Aeq,beq,lb,ub);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%ntaye aval
-
 v=reshape(x,n,d+2);
 v(:,1:2)=[];
-% k=0;
-% for i=1:n
-%     if norm(v(:,1))<10^-10
-%         k=k+1;
-%     end
-% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 filename = 'output.xlsx';
